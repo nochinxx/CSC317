607616f9398c89f7788b3f72891923d84fe8e498
nochinxx <mariojillesca@gmail.com>
2025-11-13 11:00:42 -0800

    delete and add logic

---
+++ assignments/4/scripts/calculator.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll('input[type="button"]');
+  const resultElement = document.getElementById("result");
+  console.log("Result Element:", resultElement);
+
+  let display = (value) => {
+    console.log(value);
+    if (resultElement) {
+    console.log("Current Result:", resultElement.value);
+      // Clear all
+      if (value == "AC") {
+        resultElement.value = "0";
+        return;
+      }
+      // Delete last char
+      if (value == "<") {
+        console.log("Delete Last Character");
+        if (resultElement.value.length > 1) {
+          // Pop the last char
+          resultElement.value = resultElement.value.slice(0, -1);
+          return;
+        }
+        else if (resultElement.value.length <= 1) {
+          resultElement.value = "0";
+          return;
+        }
+      }
+      resultElement.value == "0" ? resultElement.value=value : (resultElement.value += value);
+      console.log("Updated Result:", resultElement.value);
+    }
+  };
+
+  // Add event listeners to all buttons and pass their value to display function
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => display(button.value));
+  });
+});
